029c46be0de1b2da9244c708a51e4f01bc3e4be8
YourePrettyGood <preilly@princeton.edu>
2015-09-15 11:03:27 -0400

    Initial Commit

---
+++ HapSNPeval/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HapSNPeval
+//
+//  Created by Patrick Reilly on 201509/15/.
+//  Copyright (c) 2015 Patrick Reilly. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+   // insert code here...
+   std::cout << "Hello, World!\n";
+    return 0;
+}
